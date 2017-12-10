893ea841bd496ef89f050915731e9725a0977037
Soulstorm50 <figelunion@gmail.com>
2017-12-10 23:08:01 +0200

    update class MyString

--- mystring.cpp
+++ mystring.cpp
@@ -1,6 +1,65 @@
 #include "mystring.h"
 
-MyString::MyString()
+MyString::MyString() : _string(new char[1])
+{
+    //TODO:
+    _string[0] = '\0';
+}
+
+//TODO: применить в конструкторах инициализаторы / делегирование (вместо функции Initialize)
+MyString::MyString(unsigned int capacity)
+{
+    //TODO:
+}
+
+char MyString::GetCharAt(unsigned int index)
+{
+    //TODO:добавить публичный метод char GetCharAt(unsigned int index),
+    //который вернёт символ строки по указанной позиции
+}
+
+void MyString::Print()
+{
+    //TODO:показ строки на экран
+}
+
+void MyString::PrintLn()
+{
+    //TODO:показ строки на экран с переводом курсора на следующую строку
+}
+
+void MyString::GetLine()
+{
+    //TODO:ввод строки с клавиатуры
+}
+
+const char *MyString::GetCharArray() const
+{
+    //TODO:возврат константного указателя на массив символов строки
+}
+
+int MyString::CompareTo(MyString str)
+{
+    //TODO:сравнение нашей строки с какой-то другой строкой
+    //Добавить версию int CompareTo(char*)
+}
+
+void MyString::Concat(MyString str)
+{
+    //TODO:присоединить к нашей строке переданную строку
+}
+
+void MyString::Concat(char *str)
+{
+
+}
+
+void MyString::Concat(int value)
+{
+
+}
+
+void MyString::Concat(double value)
 {
 
 }
--- mystring.h
+++ mystring.h
@@ -6,9 +6,22 @@ class MyString
 {
 public:
     MyString();
+    explicit MyString(unsigned int capacity);
 
+    char GetCharAt(unsigned int index);
+    void Print();
+    void PrintLn();
+    void GetLine();
+    const char* GetCharArray() const;
+    int CompareTo(MyString str);
+    void Concat(MyString str);
+    void Concat(char* str);
+    void Concat(int value);
+    void Concat(double value);
 
 private:
+
+    char* _string;
 
 
 };
